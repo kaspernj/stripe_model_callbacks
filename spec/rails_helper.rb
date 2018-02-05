85b37c0b2f8b082356bb5bff8aecf2dbd364b50a
kaspernj <kaspernj@gmail.com>
2018-02-05 12:51:28 +0000

    Fixed issue with FactoryBot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,9 @@ require "rspec/rails"
 require "factory_bot_rails"
 require "pry-rails"
 
+FactoryBot.definition_file_paths << File.join(File.dirname(__FILE__), "factories")
+FactoryBot.find_definitions
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
