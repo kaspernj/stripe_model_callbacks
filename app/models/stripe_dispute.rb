8d7cb6b2a18ca72d67ecec9b49aa5080dff4e2c0
kaspernj <kaspernj@gmail.com>
2018-02-06 13:41:31 +0000

    Fixed various offences

--- app/models/stripe_dispute.rb
+++ app/models/stripe_dispute.rb
@@ -11,6 +11,9 @@ class StripeDispute < StripeModelCallbacks::ApplicationRecord
     )
 
     assign_evidence_attributes(object)
+    assign_evidence_cancellation_duplicate_attributes(object)
+    assign_evidence_customer_attributes(object)
+    assign_evidence_shipping_attributes(object)
     assign_evidence_details_attributes(object)
 
     StripeModelCallbacks::AttributesAssignerService.execute!(
@@ -25,17 +28,6 @@ private
     assign_attributes(
       evidence_access_activity_log: object.evidence.access_activity_log,
       evidence_billing_address: object.evidence.billing_address,
-      evidence_cancellation_policy: object.evidence.cancellation_policy,
-      evidence_cancellation_policy_disclosure: object.evidence.cancellation_policy_disclosure,
-      evidence_cancellation_rebuttal: object.evidence.cancellation_rebuttal,
-      evidence_customer_communication: object.evidence.customer_communication,
-      evidence_customer_email_address: object.evidence.customer_email_address,
-      evidence_customer_name: object.evidence.customer_name,
-      evidence_customer_purchase_ip: object.evidence.customer_purchase_ip,
-      evidence_customer_signature: object.evidence.customer_signature,
-      evidence_duplicate_charge_documentation: object.evidence.duplicate_charge_documentation,
-      evidence_duplicate_charge_explanation: object.evidence.duplicate_charge_explanation,
-      evidence_duplicate_charge_id: object.evidence.duplicate_charge_id,
       evidence_product_description: object.evidence.product_description,
       evidence_receipt: object.evidence.receipt,
       evidence_refund_policy: object.evidence.refund_policy,
@@ -43,13 +35,39 @@ private
       evidence_refund_refusal_explanation: object.evidence.refund_refusal_explanation,
       evidence_service_date: object.evidence.service_date,
       evidence_service_documentation: object.evidence.service_documentation,
+      evidence_uncategorized_file: object.evidence.uncategorized_file,
+      evidence_uncategorized_text: object.evidence.uncategorized_text
+    )
+  end
+
+  def assign_evidence_cancellation_duplicate_attributes(object)
+    assign_attributes(
+      evidence_cancellation_policy: object.evidence.cancellation_policy,
+      evidence_cancellation_policy_disclosure: object.evidence.cancellation_policy_disclosure,
+      evidence_cancellation_rebuttal: object.evidence.cancellation_rebuttal,
+      evidence_duplicate_charge_documentation: object.evidence.duplicate_charge_documentation,
+      evidence_duplicate_charge_explanation: object.evidence.duplicate_charge_explanation,
+      evidence_duplicate_charge_id: object.evidence.duplicate_charge_id
+    )
+  end
+
+  def assign_evidence_customer_attributes(object)
+    assign_attributes(
+      evidence_customer_communication: object.evidence.customer_communication,
+      evidence_customer_email_address: object.evidence.customer_email_address,
+      evidence_customer_name: object.evidence.customer_name,
+      evidence_customer_purchase_ip: object.evidence.customer_purchase_ip,
+      evidence_customer_signature: object.evidence.customer_signature
+    )
+  end
+
+  def assign_evidence_shipping_attributes(object)
+    assign_attributes(
       evidence_shipping_address: object.evidence.shipping_address,
       evidence_shipping_carrier: object.evidence.shipping_carrier,
       evidence_shipping_date: object.evidence.shipping_date,
       evidence_shipping_documentation: object.evidence.shipping_documentation,
-      evidence_shipping_tracking_number: object.evidence.shipping_tracking_number,
-      evidence_uncategorized_file: object.evidence.uncategorized_file,
-      evidence_uncategorized_text: object.evidence.uncategorized_text,
+      evidence_shipping_tracking_number: object.evidence.shipping_tracking_number
     )
   end
 
--- db/migrate/20180206130408_create_disputes.rb
+++ db/migrate/20180206130408_create_disputes.rb
@@ -10,35 +10,15 @@ class CreateDisputes < ActiveRecord::Migration[5.1]
       t.string :currency
       t.text :evidence_access_activity_log
       t.text :evidence_billing_address
-      t.text :evidence_cancellation_policy
-      t.text :evidence_cancellation_policy_disclosure
-      t.text :evidence_cancellation_rebuttal
-      t.text :evidence_customer_communication
-      t.string :evidence_customer_email_address
-      t.string :evidence_customer_name
-      t.string :evidence_customer_purchase_ip
-      t.text :evidence_customer_signature
-      t.text :evidence_duplicate_charge_documentation
-      t.text :evidence_duplicate_charge_explanation
-      t.string :evidence_duplicate_charge_id
+      evidence_cancellation_duplicate_columns(t)
+      evidence_customer_columns(t)
       t.text :evidence_product_description
       t.text :evidence_receipt
-      t.text :evidence_refund_policy
-      t.text :evidence_refund_policy_disclosure
-      t.text :evidence_refund_refusal_explanation
-      t.text :evidence_service_date
-      t.text :evidence_service_documentation
-      t.text :evidence_shipping_address
-      t.text :evidence_shipping_carrier
-      t.text :evidence_shipping_date
-      t.text :evidence_shipping_documentation
-      t.text :evidence_shipping_tracking_number
+      evidence_refund_service_columns(t)
+      evidence_shipping_columns(t)
       t.text :evidence_uncategorized_file
       t.text :evidence_uncategorized_text
-      t.datetime :evidence_details_due_by
-      t.boolean :evidence_details_has_evidence
-      t.boolean :evidence_details_past_due
-      t.integer :evidence_details_submission_count
+      evidence_details_columns(t)
       t.boolean :is_charge_refundable
       t.boolean :livemode
       t.text :metadata
@@ -46,5 +26,47 @@ class CreateDisputes < ActiveRecord::Migration[5.1]
       t.string :status
       t.timestamps
     end
+  end
+
+private
+
+  def evidence_cancellation_duplicate_columns(t)
+    t.text :evidence_cancellation_policy
+    t.text :evidence_cancellation_policy_disclosure
+    t.text :evidence_cancellation_rebuttal
+    t.text :evidence_duplicate_charge_documentation
+    t.text :evidence_duplicate_charge_explanation
+    t.string :evidence_duplicate_charge_id
+  end
+
+  def evidence_customer_columns(t)
+    t.text :evidence_customer_communication
+    t.string :evidence_customer_email_address
+    t.string :evidence_customer_name
+    t.string :evidence_customer_purchase_ip
+    t.text :evidence_customer_signature
+  end
+
+  def evidence_refund_service_columns(t)
+    t.text :evidence_refund_policy
+    t.text :evidence_refund_policy_disclosure
+    t.text :evidence_refund_refusal_explanation
+    t.text :evidence_service_date
+    t.text :evidence_service_documentation
+  end
+
+  def evidence_shipping_columns(t)
+    t.text :evidence_shipping_address
+    t.text :evidence_shipping_carrier
+    t.text :evidence_shipping_date
+    t.text :evidence_shipping_documentation
+    t.text :evidence_shipping_tracking_number
+  end
+
+  def evidence_details_columns(t)
+    t.datetime :evidence_details_due_by
+    t.boolean :evidence_details_has_evidence
+    t.boolean :evidence_details_past_due
+    t.integer :evidence_details_submission_count
   end
 end
