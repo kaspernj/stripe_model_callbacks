8e84d3dbf9e76b5f866bcb6a4b930ecfbde955c7
kaspernj <kaspernj@gmail.com>
2018-02-07 22:07:20 +0000

    Update ID when setting from Stripe

--- lib/stripe_model_callbacks/models/stripe_product.rb
+++ lib/stripe_model_callbacks/models/stripe_product.rb
@@ -10,11 +10,8 @@ class StripeProduct < StripeModelCallbacks::ApplicationRecord
   def assign_from_stripe(object)
     assign_attributes(
       active: object.active == true,
-      created: Time.zone.at(object.created),
       updated: Time.zone.at(object.updated),
       stripe_attributes: JSON.generate(object.attributes),
-      livemode: object.livemode,
-      metadata: JSON.generate(object.metadata),
       package_dimensions_height: object.package_dimensions&.height,
       package_dimensions_length: object.package_dimensions&.length,
       package_dimensions_weight: object.package_dimensions&.weight,
@@ -24,7 +21,9 @@ class StripeProduct < StripeModelCallbacks::ApplicationRecord
 
     StripeModelCallbacks::AttributesAssignerService.execute!(
       model: self, stripe_model: object,
-      attributes: %w[caption description name statement_descriptor]
+      attributes: %w[
+        caption created description id livemode name metadata statement_descriptor
+      ]
     )
   end
 end
