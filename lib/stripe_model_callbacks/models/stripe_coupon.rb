cf4cca58b3ada6719bfd1707011a7224bc7924a7
kaspernj <kaspernj@gmail.com>
2018-02-16 09:22:24 +0000

    Fixed issues with creating on Stripe

--- lib/stripe_model_callbacks/models/stripe_coupon.rb
+++ lib/stripe_model_callbacks/models/stripe_coupon.rb
@@ -12,16 +12,14 @@ class StripeCoupon < StripeModelCallbacks::ApplicationRecord
   def assign_from_stripe(object)
     assign_attributes(
       amount_off: object.amount_off ? Money.new(object.amount_off, object.currency) : nil,
-      created: Time.zone.at(object.created),
-      metadata: JSON.generate(object.metadata),
       stripe_valid: object.valid
     )
 
     StripeModelCallbacks::AttributesAssignerService.execute!(
       model: self, stripe_model: object,
       attributes: %w[
-        currency duration duration_in_months livemode max_redemptions percent_off redeem_by
-        times_redeemed
+        currency created duration duration_in_months id livemode max_redemptions metadata
+        percent_off redeem_by times_redeemed
       ]
     )
   end
