3fa86ae424c86a89ef5dab89e351a37eeb74546a
kjcommodi <kasper@commoditrader.com>
2019-06-30 06:47:13 +0000

    Added spec for error handling

---
+++ spec/requests/stripe_model_callbacks/error_handling_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+describe "error handling" do
+  let(:configuration) { StripeModelCallbacks::Configuration.current }
+  let!(:stripe_coupon) { create :stripe_coupon, stripe_id: "some-coupon" }
+  let!(:stripe_customer) { create :stripe_customer, stripe_id: "cus_CGNFgjPGtHlvXI" }
+
+  after do
+    configuration.instance_variable_set(:@on_error_callbacks, [])
+  end
+
+  it "creates an invoice" do
+    expect(StripeModelCallbacks::Invoice::UpdatedService).to receive(:reported_execute!).and_raise("BOOM!")
+
+    called = false
+    callback = proc do
+      called = true
+    end
+
+    configuration.instance_variable_set(:@on_error_callbacks, [callback])
+
+    expect { PublicActivity.with_tracking { mock_stripe_event("invoice.created") } }
+      .to raise_error(RuntimeError, "BOOM!")
+      .and change(StripeInvoice, :count).by(0)
+
+    expect(called).to eq true
+  end
+end
