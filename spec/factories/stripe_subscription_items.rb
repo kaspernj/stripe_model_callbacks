4b2c02d0f4247832c74e4521dc726067a85af73b
kaspernj <kaspernj@gmail.com>
2019-07-25 08:41:40 +0000

    Added "with_stripe_mock" trait for StripeSubscriptionItem factory

--- spec/factories/stripe_subscription_items.rb
+++ spec/factories/stripe_subscription_items.rb
@@ -4,5 +4,16 @@ FactoryBot.define do
     stripe_subscription
     stripe_plan
     quantity { 1 }
+
+    trait :with_stripe_mock do
+      after :build do |item|
+        mock_item = Stripe::SubscriptionItem.create(
+          subscription: item.stripe_subscription.stripe_id,
+          plan: item.stripe_plan.stripe_id,
+          quantity: item.quantity
+        )
+        item.assign_from_stripe(mock_item)
+      end
+    end
   end
 end
