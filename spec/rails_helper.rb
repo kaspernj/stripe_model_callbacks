b3b38a6927c26381060e5cf31d56f403241e2a73
kaspernj <kaspernj@gmail.com>
2020-03-03 20:44:02 +0000

    Fixed Rubocop warnings

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,7 +32,7 @@ require "public_activity/testing"
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!
+# ActiveRecord::Migration.maintain_test_schema!
 
 Money.locale_backend = :i18n
 
