361105c15181df4f76981052860e000e8406157b
kaspernj <kaspernj@gmail.com>
2020-03-06 15:43:31 +0000

    Coupon and discount fixes

--- lib/stripe_model_callbacks/factories/stripe_coupons.rb
+++ lib/stripe_model_callbacks/factories/stripe_coupons.rb
@@ -1,5 +1,21 @@
 FactoryBot.define do
   factory :stripe_coupon do
     sequence(:stripe_id) { |n| "stripe-coupon-#{n}" }
+
+    trait :with_stripe_mock do
+      duration { "repeating" }
+      duration_in_months { 1 }
+
+      after :create do |stripe_coupon|
+        mock_coupon = Stripe::Coupon.create(
+          duration: stripe_coupon.duration,
+          duration_in_months: stripe_coupon.duration_in_months,
+          id: stripe_coupon.stripe_id,
+          percent_off: stripe_coupon.percent_off
+        )
+        stripe_coupon.assign_from_stripe(mock_coupon)
+        stripe_coupon.save!
+      end
+    end
   end
 end
