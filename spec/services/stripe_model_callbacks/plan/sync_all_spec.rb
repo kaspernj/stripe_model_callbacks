3564122ddf9897e72f54853869d62e75a5ea5f09
kaspernj <kaspernj@gmail.com>
2020-03-17 19:08:18 +0100

    Fixed specs

--- spec/services/stripe_model_callbacks/plan/sync_all_spec.rb
+++ spec/services/stripe_model_callbacks/plan/sync_all_spec.rb
@@ -1,10 +1,10 @@
 require "rails_helper"
 
 describe StripeModelCallbacks::Plan::SyncAll, :stripe_mock do
-  let(:stripe_product) { create :stripe_product }
+  let(:stripe_product) { create :stripe_product, :with_stripe_mock }
 
   it "creates the model locally" do
-    Stripe::Plan.create(amount: 1000, id: "test-plan", currency: "usd", name: "Test plan", interval: 1, product: stripe_product.stripe_id)
+    Stripe::Plan.create(amount: 1000, id: "test-plan", currency: "usd", name: "Test plan", interval: "month", product: stripe_product.stripe_id)
 
     expect { StripeModelCallbacks::Plan::SyncAll.execute! }
       .to change(StripePlan, :count).by(1)
@@ -13,7 +13,7 @@ describe StripeModelCallbacks::Plan::SyncAll, :stripe_mock do
 
     expect(created_plan).to have_attributes(
       currency: "usd",
-      interval: "1",
+      interval: "month",
       stripe_id: "test-plan",
       stripe_product_id: stripe_product.stripe_id
     )
--- spec/services/stripe_model_callbacks/sync_from_stripe_spec.rb
+++ spec/services/stripe_model_callbacks/sync_from_stripe_spec.rb
@@ -1,10 +1,10 @@
 require "rails_helper"
 
 describe StripeModelCallbacks::SyncFromStripe, :stripe_mock do
-  let(:stripe_product) { create :stripe_product }
+  let(:stripe_product) { create :stripe_product, :with_stripe_mock }
 
   it "creates the model locally" do
-    mock_plan = Stripe::Plan.create(amount: 1000, id: "test-plan", currency: "usd", name: "Test plan", interval: 1, product: stripe_product.stripe_id)
+    mock_plan = Stripe::Plan.create(amount: 1000, id: "test-plan", currency: "usd", name: "Test plan", interval: "month", product: stripe_product.stripe_id)
 
     expect { StripeModelCallbacks::SyncFromStripe.execute!(stripe_object: mock_plan) }
       .to change(StripePlan, :count).by(1)
@@ -13,7 +13,7 @@ describe StripeModelCallbacks::SyncFromStripe, :stripe_mock do
 
     expect(created_plan).to have_attributes(
       currency: "usd",
-      interval: "1",
+      interval: "month",
       stripe_id: "test-plan",
       stripe_product_id: stripe_product.stripe_id
     )
