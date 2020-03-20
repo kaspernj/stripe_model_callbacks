d23dcc454d35c95c71020df25d23856348084567
Robin Wunderlin <robin@commoditrader.com>
2020-03-20 11:42:51 +0100

    Check for optional fields for stripe_product

--- lib/stripe_model_callbacks/models/stripe_product.rb
+++ lib/stripe_model_callbacks/models/stripe_product.rb
@@ -8,15 +8,21 @@ class StripeProduct < StripeModelCallbacks::ApplicationRecord
   def assign_from_stripe(object)
     assign_attributes(
       active: object.active == true,
-      updated: Time.zone.at(object.updated),
-      stripe_attributes: JSON.generate(object.attributes),
-      package_dimensions_height: object.package_dimensions&.height,
-      package_dimensions_length: object.package_dimensions&.length,
-      package_dimensions_weight: object.package_dimensions&.weight,
-      package_dimensions_width: object.package_dimensions&.width,
       product_type: object.type,
-      shippable: object.shippable == true
+      stripe_attributes: JSON.generate(object.attributes),
+      updated: Time.zone.at(object.updated)
     )
+
+    if object.respond_to?(:package_dimensions)
+      assign_attributes(
+        package_dimensions_height: object.package_dimensions&.height,
+        package_dimensions_length: object.package_dimensions&.length,
+        package_dimensions_weight: object.package_dimensions&.weight,
+        package_dimensions_width: object.package_dimensions&.width
+      )
+    end
+
+    assign_attributes(shippable: object.shippable == true) if object.respond_to?(:shippable)
 
     StripeModelCallbacks::AttributesAssignerService.execute!(
       model: self, stripe_model: object,
