8bdaf693c5f7a0d528e69e28e8c8f9dd35a65e31
Adam Malinowski <adam.malinowski@packhelp.com>
2020-05-14 16:06:37 +0200

    refactor invoice update service

--- app/services/stripe_model_callbacks/invoice/updated_service.rb
+++ app/services/stripe_model_callbacks/invoice/updated_service.rb
@@ -1,10 +1,18 @@
 class StripeModelCallbacks::Invoice::UpdatedService < StripeModelCallbacks::BaseEventService
+  ACTIVITY_TYPES = {
+    "invoice.payment_failed": :payment_failed,
+    "invoice.payment_succeeded": :payment_succeeded,
+    "invoice.sent": :sent,
+    "invoice.upcoming": :upcoming
+  }.freeze.with_indifferent_access
+  private_constant :ACTIVITY_TYPES
+
   def execute
   invoice.assign_from_stripe(object)
   return success_actions if invoice.save
 
   fail!(invoice.errors.full_messages)
-  rescue ActiveRecord::RecordNotUnique, PG::UniqueViolation
+  rescue ActiveRecord::RecordNotUnique, PG::UniqueViolation, SQLite3::ConstraintException
     return succeed! if event.type == "invoice.created"
 
     raise ActiveRecord::RecordNotUnique
@@ -12,19 +20,16 @@ class StripeModelCallbacks::Invoice::UpdatedService < StripeModelCallbacks::Base
 
 private
 
+  def success_actions
+    create_activity
+    succeed!
+  end
+
   def create_activity
-    invoice.create_activity :payment_failed if event.type == "invoice.payment_failed"
-    invoice.create_activity :payment_succeeded if event.type == "invoice.payment_succeeded"
-    invoice.create_activity :sent if event.type == "invoice.sent"
-    invoice.create_activity :upcoming if event.type == "invoice.upcoming"
+    invoice.create_activity(ACTIVITY_TYPES[event.type]) if ACTIVITY_TYPES.fetch(event.type, nil)
   end
 
   def invoice
     @invoice ||= StripeInvoice.find_or_initialize_by(stripe_id: object.id)
-  end
-
-  def success_actions
-    create_activity
-    succeed!
   end
 end
--- spec/services/stripe_model_callbacks/invoice/updated_service_spec.rb
+++ spec/services/stripe_model_callbacks/invoice/updated_service_spec.rb
@@ -7,27 +7,36 @@ describe StripeModelCallbacks::Invoice::UpdatedService do
   let(:event_payload) { File.read("spec/fixtures/stripe/invoice/created.json") }
   let(:event_values) do
     {
-      id: "evt_charge_succeeded",
+      id: "evt_1GiGbaGJAmuZ9ZZZbKpF4hGj",
       type: event_type,
       data: JSON.parse(event_payload, symbolize_names: true)
     }
+  end
+  let(:stripe_id) { "in_1GiGbYGJAmuZ9ZZZmstXLFQx" }
+
+  shared_examples "do not create new invoice" do
+    it "does not create new stripe invoice" do
+      expect { updated_service }.not_to change(StripeInvoice.all, :size)
+    end
+  end
+
+  shared_examples "creates new invoice" do
+    it "creates stripe invoice" do
+      expect { updated_service }.to change { StripeInvoice.all.size }.by(1)
+    end
   end
 
   context "when 'invoice.created' event was sent" do
     let(:event_type) { "invoice.created" }
 
     context "when invoice don't exist" do
-      it "creates stripe invoice" do
-        expect { updated_service }.to change { StripeInvoice.all.size }.by(1)
-      end
+      it_behaves_like "creates new invoice"
     end
 
     context "when invoice already exist" do
-      let!(:stripe_invoice) { create(:stripe_invoice, stripe_id: "in_1GiGbYGJAmuZ9ZZZmstXLFQx") }
+      let!(:stripe_invoice) { create(:stripe_invoice, stripe_id: stripe_id) }
 
-      it "does not create new stripe invoice" do
-        expect { updated_service }.not_to change(StripeInvoice.all, :size)
-      end
+      it_behaves_like "do not create new invoice"
     end
   end
 
@@ -35,9 +44,7 @@ describe StripeModelCallbacks::Invoice::UpdatedService do
     let(:event_type) { "invoice.payment_succeeded" }
 
     context "when invoice don't exist" do
-      it "creates stripe invoice" do
-        expect { updated_service }.to change { StripeInvoice.all.size }.by(1)
-      end
+      it_behaves_like "creates new invoice"
 
       it "creates stripe invoice activity" do
         updated_service
@@ -46,10 +53,12 @@ describe StripeModelCallbacks::Invoice::UpdatedService do
     end
 
     context "when invoice already exist" do
-      let!(:stripe_invoice) { create(:stripe_invoice, stripe_id: "in_1GiGbYGJAmuZ9ZZZmstXLFQx") }
+      let!(:stripe_invoice) { create(:stripe_invoice, stripe_id: stripe_id) }
 
-      it "does not create new stripe invoice" do
-        expect { updated_service }.not_to change(StripeInvoice.all, :size)
+      before { allow(StripeInvoice).to receive(:find_or_initialize_by).with(stripe_id: stripe_id) { StripeInvoice.new } }
+
+      it "raises an error" do
+        expect { updated_service }.to raise_error(ActiveRecord::RecordNotUnique)
       end
     end
   end
