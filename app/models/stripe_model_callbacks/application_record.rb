0c5d0107b6704cd9c526d27a4dcdf4abc0aa0f7a
kaspernj <k@spernj.org>
2021-04-29 10:36:10 +0200

    Fixed offence

--- app/models/stripe_model_callbacks/application_record.rb
+++ app/models/stripe_model_callbacks/application_record.rb
@@ -14,7 +14,7 @@ class StripeModelCallbacks::ApplicationRecord < ActiveRecord::Base
 
     allowed ||= [stripe_class]
 
-    raise "Expected #{object.class.name} to be a #{allowed.map(&:name).join(", ")}" unless allowed.any? { |stripe_class| object.is_a?(stripe_class) }
+    raise "Expected #{object.class.name} to be a #{allowed.map(&:name).join(", ")}" unless allowed.any?(stripe_class)
   end
 
   def check_object_is_stripe_class(object, allowed = nil)
