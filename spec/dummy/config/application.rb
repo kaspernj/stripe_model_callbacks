fd0965dd42ba318e38a3246b54898c21fbe0d22b
kaspernj <k@spernj.org>
2022-01-01 07:52:28 +0000

    Removed debug puts

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -18,10 +18,8 @@ module Dummy; end
 class Dummy::Application < Rails::Application
   # Initialize configuration defaults for originally generated Rails version.
   if Gem.loaded_specs["rails"].version.to_s.start_with?("7.")
-    puts "LOAD 7 DEFAULTS"
     config.load_defaults 7.0
   else
-    puts "LOAD 6 DEFAULTS"
     config.load_defaults 6.0
   end
 
