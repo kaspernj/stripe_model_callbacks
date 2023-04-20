600909792e41df8b62a1e307e53019be938cb25d
kaspernj <k@spernj.org>
2023-04-20 11:35:44 +0000

    Fixed offence

--- app/services/stripe_model_callbacks/payment_method/updated_service.rb
+++ app/services/stripe_model_callbacks/payment_method/updated_service.rb
@@ -4,14 +4,16 @@ class StripeModelCallbacks::PaymentMethod::UpdatedService < StripeModelCallbacks
     payment_method.assign_from_stripe(object)
 
     if payment_method.save
-      payment_method.create_activity :attached if event.type == "payment_method.attached"
-      payment_method.create_activity :automatically_updated if event.type == "payment_method.automatically_updated"
-      payment_method.create_activity :card_automatically_updated if event.type == "payment_method.card_automatically_updated"
-      payment_method.create_activity :detached if event.type == "payment_method.detached"
-      payment_method.create_activity :updated if event.type == "payment_method.updated"
+      create_activity!(payment_method)
       succeed!
     else
       fail! payment_method.errors.full_messages
     end
+  end
+
+  def create_activity!(payment_method)
+    match = event.type.match(/\Apayment_method\.(.+)$/)
+    activity_type = match[1].to_sym
+    payment_method.create_activity activity_type
   end
 end
