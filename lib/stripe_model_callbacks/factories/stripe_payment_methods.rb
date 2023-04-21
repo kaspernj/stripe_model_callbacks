df784e599c78b7c1a9bbc031a7d11d971d9d4933
kaspernj <k@spernj.org>
2023-04-21 11:26:20 +0000

    Added with_stripe_mock trait

--- lib/stripe_model_callbacks/factories/stripe_payment_methods.rb
+++ lib/stripe_model_callbacks/factories/stripe_payment_methods.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :stripe_payment_method do
     sequence(:stripe_id) { |n| "stripe-payment-method-#{n}" }
+
+    trait :with_stripe_mock do
+      after :create do |stripe_payment_method|
+        mock_payment_method = Stripe::PaymentMethod.create(customer: stripe_payment_method.customer, id: stripe_payment_method.stripe_id, type: "card")
+        stripe_payment_method.assign_from_stripe(mock_payment_method)
+        stripe_payment_method.save!
+      end
+    end
   end
 end
