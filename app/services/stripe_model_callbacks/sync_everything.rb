e52b584b621189abdb1d523e847b3ec0dd45ffdb
kaspernj <k@spernj.org>
2023-04-29 11:35:11 +0000

    Sync a lot more models

--- app/services/stripe_model_callbacks/sync_everything.rb
+++ app/services/stripe_model_callbacks/sync_everything.rb
@@ -1,8 +1,22 @@
 class StripeModelCallbacks::SyncEverything < StripeModelCallbacks::BaseService
   def perform
-    StripeModelCallbacks::Coupon::SyncAll.execute!
-    StripeModelCallbacks::Plan::SyncAll.execute!
+    stripe_classes = [Stripe::Customer, Stripe::Coupon, Stripe::Invoice, Stripe::Plan, Stripe::PaymentIntent, Stripe::Subscription]
+    stripe_classes.each do |stripe_class|
+      stripe_class.list.each do |stripe_object|
+        StripeModelCallbacks::SyncFromStripe.execute!(stripe_object: stripe_object)
+
+        if stripe_class == Stripe::Customer
+          sync_stripe_objects(Stripe::PaymentMethod.list(customer: stripe_object.id))
+        end
+      end
+    end
 
     succeed!
+  end
+
+  def sync_stripe_objects(stripe_objects)
+    stripe_objects.each do |stripe_object|
+      StripeModelCallbacks::SyncFromStripe.execute!(stripe_object: stripe_object)
+    end
   end
 end
