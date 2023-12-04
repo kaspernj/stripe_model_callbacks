618fe3bf8859a8cc595bf4d26f0815fa5bcf2cf8
kaspernj <k@spernj.org>
2023-12-04 06:51:50 +0000

    Fixed offences

--- db/migrate/20180206151132_change_activities_trackable_id_to_string.rb
+++ db/migrate/20180206151132_change_activities_trackable_id_to_string.rb
@@ -1,5 +1,5 @@
 class ChangeActivitiesTrackableIdToString < ActiveRecord::Migration[5.1]
   def change
-    change_column :activities, :trackable_id, :string  # rubocop:disable Rails/ReversibleMigration
+    change_column :activities, :trackable_id, :string # rubocop:disable Rails/ReversibleMigration
   end
 end
