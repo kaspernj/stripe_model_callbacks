6d056e645f957487a2d1356921c01b9b69b35c86
kaspernj <k@spernj.org>
2024-06-14 15:03:05 +0000

    Added Stripe mock support to factory

--- lib/stripe_model_callbacks/factories/stripe_tax_rates.rb
+++ lib/stripe_model_callbacks/factories/stripe_tax_rates.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :stripe_tax_rate do
     sequence(:stripe_id) { |n| "stripe-tax-rate-#{n}" }
+
+    trait :with_stripe_mock do
+      after :create do |stripe_tax_rate|
+        mock_tax_rate = Stripe::TaxRate.create(
+          id: stripe_tax_rate.stripe_id
+        )
+        stripe_tax_rate.assign_from_stripe(mock_tax_rate)
+        stripe_tax_rate.save!
+      end
+    end
   end
 end
