bd1a51f858464ccdfb59f52df398f6491ac9ee80
kaspernj <kasper@diestoeckels.de>
2026-01-02 21:43:48 +0000

    Handle source transaction created events

--- spec/requests/stripe_model_callbacks/source/transaction_created_spec.rb
+++ spec/requests/stripe_model_callbacks/source/transaction_created_spec.rb
@@ -1,14 +1,18 @@
 require "rails_helper"
 
 describe "source transaction created" do
+  let!(:source) { create :stripe_source, stripe_id: "src_00000000000000" }
+
   describe "#execute!" do
-    it "creates a new transaction" do
-      expect { mock_stripe_event("source.transaction_created") }
+    it "creates an activity for the source" do
+      expect { mock_stripe_event("source.transaction.created") }
         .to change(StripeSource, :count).by(0)
+        .and change(
+          ActiveRecordAuditable::Audit.where_type("StripeSource").where_action("transaction_created"),
+          :count
+        ).by(1)
 
       expect(response).to have_http_status :ok
-
-      # FIXME: Write more specs
     end
   end
 end
