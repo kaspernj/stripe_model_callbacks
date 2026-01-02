1b21968402a262445b2e76379cee0d833034ae63
kaspernj <kasper@diestoeckels.de>
2026-01-02 22:31:59 +0000

    Fix base service advisory lock spec

--- spec/services/stripe_model_callbacks/base_service_spec.rb
+++ spec/services/stripe_model_callbacks/base_service_spec.rb
@@ -3,13 +3,26 @@ require "rails_helper"
 describe StripeModelCallbacks::BaseService do
   let(:service_class) do
     Class.new(described_class) do
+      attr_reader :event
+
+      def initialize(event:)
+        @event = event
+      end
+
       def perform
         succeed!
       end
     end
   end
-  let(:stripe_object) { double("StripeObject", object: "invoice", id: "in_123") }
-  let(:stripe_event) { double("StripeEvent", data: double(object: stripe_object)) }
+  let(:stripe_object) { instance_double(FakeStripeObject, object: "invoice", id: "in_123") }
+  let(:stripe_data) { instance_double(FakeStripeEventData, object: stripe_object) }
+  let(:stripe_event) { instance_double(FakeStripeEvent, data: stripe_data) }
+
+  before do
+    stub_const("FakeStripeObject", Struct.new(:object, :id))
+    stub_const("FakeStripeEventData", Struct.new(:object))
+    stub_const("FakeStripeEvent", Struct.new(:data))
+  end
 
   it "acquires an advisory lock with a timeout" do
     expect(StripeModelCallbacks::ApplicationRecord).to receive(:with_advisory_lock!)
